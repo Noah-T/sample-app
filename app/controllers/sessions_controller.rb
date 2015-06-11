1e785410c49a66908e281bd607ceddabda4ca74c
Noah Teshu <nteshu@gmail.com>
2015-06-11 17:52:07 -0400

    add current user method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,9 @@ class SessionsController < ApplicationController
     #if user is found, check password with bcrypt
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
+      log_in user
+      #show their profile
+      redirect_to user
     else
     	#flash.now will show a message and disappear on the next page. flash will only disappear after it persits for one redirect (render doesn't count as a redirect)
     	flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
