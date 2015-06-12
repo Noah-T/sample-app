8344ea26b23cf5c5d4fd119aae0971bbc64fd1dd
Noah Teshu <nteshu@gmail.com>
2015-06-12 16:05:32 -0400

    add login helper

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test "test login" do 
   	#go to login
   	get '/login'
@@ -17,5 +21,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   	#now the flash message should be empty
   	assert flash.empty?
 
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
