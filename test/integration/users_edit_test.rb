9205ae9a6d6d78d839c1c1ecb2821ea19ce8507a
Noah Teshu <nteshu@gmail.com>
2015-06-28 15:02:43 -0400

    fix edit user test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,31 +6,33 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  # test "unsuccessful edit" do
-  #   get edit_user_path(@user)
-  #   #get edit_user_path not opening up edit form. Maybe because of use of fixture?
-  #   #assert_template 'users/edit'
-  #   patch user_path(@user), user: { name:  "",
-  #                                   email: "foo@invalid",
-  #                                   password:              "foo",
-  #                                   password_confirmation: "bar" }
-  #   assert_template 'users/edit'
-  # end
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    #get edit_user_path not opening up edit form. Maybe because of use of fixture?
+    #assert_template 'users/edit'
+    patch user_path(@user), user: { name:  "",
+                                    email: "foo@invalid",
+                                    password:              "foo",
+                                    password_confirmation: "bar" }
+    assert_template 'users/edit'
+  end
 
-  # test "successful edit" do
-  # 	#get edit_user_path not opening up edit form. Maybe because of use of fixture?
-  #   #get edit_user_path(@user)
-  #   assert_template 'users/edit'
-  #   name  = "Foo Bar"
-  #   email = "foo@bar.com"
-  #   patch user_path(@user), user: { name:  name,
-  #                                   email: email,
-  #                                   password:              "",
-  #                                   password_confirmation: "" }
-  #   assert_not flash.empty?
-  #   assert_redirected_to @user
-  #   @user.reload
-  #   assert_equal name,  @user.name
-  #   assert_equal email, @user.email
-  # end
+  test "successful edit" do
+  	
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), user: { name:  name,
+                                    email: email,
+                                    password:              "",
+                                    password_confirmation: "" }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
 end
