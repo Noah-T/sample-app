74617ad62bdc6084909697241e0122fb24339d3a
Noah Teshu <nteshu@gmail.com>
2015-06-30 16:47:19 -0400

    add second user for testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,9 +72,10 @@ class UsersController < ApplicationController
       end
     end
 
+    # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 
     def admin_user
